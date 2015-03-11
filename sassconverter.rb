0df383120294da3a3da18b5b305487d955a57542
eduard herman <herman_eduard2000@yahoo.com>
2015-03-11 09:58:50 +0100

    v1.1
    check if scss file is found and run the converter, else just throw a
    message

--- sassconverter.rb
+++ sassconverter.rb
@@ -3,12 +3,21 @@ require 'sass'
 puts "Enter sass file location"
 pathname = gets.chomp
 
+if !Dir.glob("#{pathname}/*.scss").empty? 
 
 Dir.glob("#{pathname}/*.scss") do |scss_file|
+puts "OK I found this file #{scss_file}"	
 css = Sass::Engine.for_file(scss_file, {}).render
 File.open("#{scss_file[0..-6]}.css", "wb") {|f| f.write(css) }
 puts "Bravo, #{scss_file[0..-6]} was generated for you"
+
 end
 
-puts "\nThanks for using the script.\nCheck your scss folder for output and Hit enter to exit"
+else 
+
+	puts "I cannot find any scss files! \n Run the script again and write a valid pathname \n which contains at least one .scss file"
+
+end	
+
+puts "\nThanks for using the script."
 gets
