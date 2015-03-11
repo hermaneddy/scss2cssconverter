a68ab853e17fd69316174851e5a9df86cd472674
Machinas SL <eduard@machinas.com>
2015-03-11 09:25:20 +0100

    v1
    Asks for path to scss file and converts eveything into css

---
+++ sassconverter.rb
@@ -0,0 +1,15 @@
+require 'sass'
+
+puts "Enter sass file location"
+pathname = gets.chomp
+
+
+Dir.glob("#{pathname}/*.scss") do |scss_file|
+css = Sass::Engine.for_file(scss_file, {}).render
+File.open("#{scss_file[0..-6]}.css", "wb") {|f| f.write(css) }
+puts "Bravo, #{scss_file[0..-6]} was generated for you"
+end
+
+puts "\nThanks for using the script.\nCheck your scss folder for output and Hit enter to exit"
+
+gets
